f2efa1f8a1aa141e2b7ffb41986ce9a90e89d57e
Nicolas Artman <nicolasartman@gmail.com>
2012-06-17 14:32:53 -0700

    refactoring drag and drop

--- falling_frontend/app.js
+++ falling_frontend/app.js
@@ -1,4 +1,12 @@
-"use strict"
+/*global 
+ jquery:true,
+ $:true,
+ us: true,
+ _: true,
+ document: true,
+ angular: true
+*/
+
 
 var us = _.noConflict()
 
@@ -6,27 +14,48 @@ var us = _.noConflict()
 var state = {}
 var myPlayerNumber = 0
 
-$(document).ready(function () {
+$(document).ready(function ($) {
   $(document).bind('mousemove', function (event) {
-    var x, y
-    x = event.pageX - ($('#hand').width() / 2.0)
-    y = event.pageY - ($('#hand').height() / 2.0)
+    var x = (event.pageX - ($('#hand').width() / 2.0)),
+    y = (event.pageY - ($('#hand').height() / 2.0))
     $('#hand').offset({left: x, top: y })
+  })
+  .bind('mouseup', function (cursor) {
+    $('.rider').each(function (index) {
+      var rider = $(this)
+      // hit test
+      if ((cursor.pageX > rider.offset().left && // left edge
+          cursor.pageX < rider.offset().left + rider.width()) && // right edge
+          (cursor.pageY > rider.offset().top && // top edge
+          cursor.pageY < rider.offset().top + rider.height())) { // bottom edge
+        var $rider = angular.element(this).scope()
+        var $hand = angular.element($('#hand')).scope()
+        // If card is successfully played, clear the hand
+        // TODO: account for invalidated plays
+        if ($rider.playCard($hand.myHand)) {
+          $hand.clearHand()
+          console.log("drop callback")
+          console.log($hand)
+          // Don't let player draw another card until the move is validated
+          $hand.pendingValidation = true
+        }
+      }
+    })
   })
 })
 
 
-function dealCard(playerNumber, stackNumber, card) {
-  state.players[playerNumber].stacks[stackNumber].push(card)
-  // angular.$apply()
-}
+// function dealCard(playerNumber, stackNumber, card) {
+//   state.players[playerNumber].stacks[stackNumber].push(card)
+//   // angular.$apply()
+// }
 
 function PlayerListController($scope) {
   var playerCount = 5
   state.players = []
   $scope.players = state.players
   
-  for (var playerNum = 0; playerNum < playerCount; playerNum++){
+  for (var playerNum = 0; playerNum < playerCount; playerNum++) {
     state.players.push({
       playerNumber: playerNum,
       hand: null,
@@ -48,6 +77,13 @@ function PlayerListController($scope) {
   }
 }
 
+function HandController($scope) {
+  $scope.clearHand = function () {
+    console.log($scope)
+    // $scope.myHand = null
+  }
+}
+
 function CardListController($scope) {
   $scope.cards = [
     { number: 1 },
@@ -64,12 +100,13 @@ function RiderController($scope) {
   
   $scope.rider = $scope.$parent.player.rider
   
-  $scope.playCard = function ($riderScope, $cardScope) {
-    console.log($riderScope)
-    console.log($cardScope)
-    if (!$riderScope.rider.card) {
-      $riderScope.rider.card = $cardScope.card
+  $scope.playCard = function (card) {
+    // console.log("playCard"); console.log($scope)
+    if (!$scope.rider.card) {
+      $scope.rider.card = card
       return true
+    } else {
+      return false
     }
   }
 }
