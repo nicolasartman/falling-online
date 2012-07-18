fff55d1fca72f06717e59b7efc767a76d24f45ce
Nicolas Artman <nicolasartman@gmail.com>
2012-07-18 00:11:34 -0700

    add simple websocket dummy server for testing and jshint for pre-commit code linting

---
+++ falling_frontend/test/mock-ws-server.js
@@ -0,0 +1,32 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+var socket = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: true
+});
+
+socket.on('connect', function(connection) {
+  console.log((new Date()) + ' Connection established.');
+  connection.on('message', function(message) {
+    console.log('Received Message: ' + message.utf8Data);
+    connection.sendUTF(message.utf8Data);
+  });
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+});
