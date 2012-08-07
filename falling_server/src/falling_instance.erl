886d1ea2b646abb57c92f10765c7390b33fdb581
Kelly Wong <kelly@bluejeans.com>
2012-08-06 22:18:14 -0700

    Change to erlang send_after from timer:apply_after

--- falling_server/src/falling_instance.erl
+++ falling_server/src/falling_instance.erl
@@ -3,8 +3,7 @@
 %-include("include/fgame.hrl").
 -include("include/game_meta.hrl").
 -export([start_link/0]).
--export([send_deal/2]).
--export([init/1, handle_call/3]).
+-export([init/1, handle_call/3, handle_info/2]).
 
 start_link() ->
   gen_server:start_link(falling_instance, [], []).
@@ -14,20 +13,20 @@ init([]) ->
 
 handle_call({start_game, Speed, Count}, _From, {Meta, none}) ->
   Game = falling:new_game(Count),
-  timer:apply_after(Speed, ?MODULE, send_deal, [self(), Speed]),
+  erlang:send_after(Speed, self(), {send_deal, Speed}),
   {reply, ok, {Meta, Game}};
+handle_call(listen, {From, _Tag}, {Meta, Game}) ->
+  {reply, ok, {Meta#game_meta{listeners = [From | Meta#game_meta.listeners]}, Game}};
 handle_call(state, _From, {Meta, Game}) ->
   {reply, Game, {Meta, Game}};
 handle_call(_Any, _From, {Meta, {game_over, Winner, Game}}) ->
   {reply, {game_over, Winner, []}, {Meta, {game_over, Winner, Game}}};
 handle_call(deal, _From, {Meta, Game}) ->
-  case falling:deal(Game) of
+  case do_deal(Game, Meta) of
+    {game_over, Winner, Deltas, NextGame} ->
+      {reply, {game_over, Winner, Deltas}, {Meta, {game_over, Winner, NextGame}}};
     {NextGame, Deltas} ->
-      broadcast_deltas(Meta, Deltas),
-      {reply, Deltas, {Meta, NextGame}};
-    { game_over, Winner, Deltas, NextGame } ->
-      broadcast_deltas(Meta, {game_over, Winner, Deltas}),
-      {reply, {game_over, Winner, Deltas}, {Meta, {game_over, Winner, NextGame}}}
+      {reply, Deltas, {Meta, NextGame}}
   end;
 handle_call({draw, PlayerId, StackId}, _From, {Meta, Game}) ->
   {NextGame, Deltas} = falling:draw_card(PlayerId, StackId, Game),
@@ -38,14 +37,26 @@ handle_call({play_card, FromPlayerId, ToPlayerId}, _From, {Meta, Game}) ->
   broadcast_deltas(Meta, Deltas),
   {reply, Deltas, {Meta, NextGame}}.
 
-send_deal(GamePid, Speed) ->
-  case gen_server:call(GamePid, deal) of
-    {game_over, Winner, _Deltas} ->
+handle_info({send_deal, Speed}, {Meta, Game}) ->
+  case do_deal(Game, Meta) of
+    { game_over, Winner, Deltas, NextGame } ->  
+      io:format("Card dealt ~w~n", [Deltas]),
       io:format("Game over, player ~w wins~n", [Winner]),
-      ok;
-    DeltaList ->
-      io:format("Card dealt ~w~n", [DeltaList]),
-      timer:apply_after(Speed, ?MODULE, send_deal, [GamePid, Speed])
+      { noreply, {Meta, {game_over, Winner, NextGame}}};
+    {NextGame, Deltas} ->
+      io:format("Card dealt ~w~n", [Deltas]),
+      erlang:send_after(Speed, self(), {send_deal, Speed}),
+      { noreply, {Meta, NextGame}}
+  end.
+
+do_deal(Game, Meta) ->  
+  case falling:deal(Game) of
+    { game_over, Winner, Deltas, NextGame } ->
+      broadcast_deltas(Meta, {game_over, Winner, Deltas}),
+      {game_over, Winner, Deltas, NextGame};
+    {NextGame, Deltas} ->
+      broadcast_deltas(Meta, Deltas),
+      {NextGame, Deltas}
   end.
 
 %Don't broadcast rejection messages
